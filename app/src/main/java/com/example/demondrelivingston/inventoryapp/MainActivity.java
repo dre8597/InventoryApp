ee1b5fa6585ff147b4c88ca021c6d55fa3988117
Demondre Livingston <livingstondemondre7@gmail.com>
2018-01-03 19:40:43 -0500

    add color and string values finished mainActivity and ProductContract Repaired Gradle Completed skeleton of xml layouts began CursorAdapter

--- app/src/main/java/com/example/demondrelivingston/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/demondrelivingston/inventoryapp/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.demondrelivingston.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.demondrelivingston.inventoryapp.data.ProductContract.ProductEntry;
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    /**
+     * Identifier for the pet data loader
+     */
+    private static final int PRODUCT_LOADER = 0;
+
+    /**
+     * Adapter for the ListView
+     */
+    ProductCursorAdapter mCursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup FAB to open DetailActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Find the ListView which will be populated with the product data
+        ListView productListView = (ListView) findViewById(R.id.list);
+
+        //Find and set empty view on the ListView,
+        // so that it only shows when list 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
+        //setup an Adapter to create a list item for each row of product data in the Cursor.
+        //There is no product data yet(until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        //setup item click listener
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                intent.setData(currentProductUri);
+                startActivity(intent);
+            }
+        });
+
+        //kick off the loader
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        //Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_IMAGE,
+                ProductEntry.COLUMN_PRODUCT_AMOUNT};
+
+        //This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,//Parent activity context
+                ProductEntry.CONTENT_URI,//Provider content URI to query
+                projection,//columns to include in the resulting Cursor
+                null,//No selection clause
+                null,//No selection arguments
+                null);//Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        //Update {@ProductCursorAdapter} with this new cursor containing updated product data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        //Callback clled when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
--- app/src/main/java/com/example/demondrelivingston/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/demondrelivingston/inventoryapp/ProductCursorAdapter.java
@@ -1,8 +1,28 @@
 package com.example.demondrelivingston.inventoryapp;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
 /**
  * Created by demondrelivingston on 1/2/18.
  */
 
-public class ProductCursorAdapter {
+public class ProductCursorAdapter extends CursorAdapter{
+
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0/*flags*/);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
